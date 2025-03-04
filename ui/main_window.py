3951a5b8d24cade71c0e9e986baa93a3705a1e79
mrtn <mwtr@tuta.io>
2025-03-04 11:54:30 +0100

    updated license

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,21 +1,8 @@
-"""
-OpenCarve - Image to Gcode-Converter
-Copyright (c) 2025 Martin Winter
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-"""
+#!/usr/bin/python
+# This file is part of OpenCarvePython, written by:
+# (C) Martin Winter Email: mwtr@tuta.io
+# License: GPL V2 (see LICENSE)
+# Original Code can be found at: https://github.com/wytr/OpenCarvePython
 
 import os
 import importlib
